0232ad2d7077c2aedc72cce34bca8975df97f985
Yurii <stejlyura19@gmail.com>
2026-04-24 13:21:37 +0300

    build: switch svelte adapter from auto to cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto'
-//import adapter from '@sveltejs/adapter-cloudflare';
+// import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
